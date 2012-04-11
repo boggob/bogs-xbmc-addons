4d42c7441dda89a0b8ee93df7b797797bac4c44f
bog.gob@gmail.com <bog.gob@gmail.com@84c78e49-bb0b-d80c-ffa2-b598af091f5d>
2012-04-11 21:45:46 +0000

    cleaned up xml

--- trunk/md5.py
+++ trunk/md5.py
@@ -1,13 +1,27 @@
 import hashlib
 import os.path,sys
+import glob
+import pprint
 
+def main():
+	di	= sys.path[0]
+	
+	addons = {}
+	for fin in sorted(glob.glob(di + "/*")):
+		print fin
+		if os.path.isdir(fin) and os.path.abspath(fin) != os.path.abspath(di):
+			with open(fin + "/addon.xml", "r") as fi:
+				addons[fin] = fi.read()
+	
+	pprint.pprint( addons)
+	with open(di + "/addons.xml", "w") as fo:
+		fo.write("<addons>")	
+		for k,v in sorted(addons.iteritems()):
+			fo.write("\n  " + "\n".join("  " + st for st in v.replace('<?xml version="1.0" encoding="utf-8"?>', '').split("\n")).strip())	
+		fo.write("\n</addons>")	
+	
+	with open(di + "/addons.xml") as fi:	
+		with open(di + "/addons.xml.md5", "w") as fo:
+			fo.write(hashlib.md5(fi.read()).hexdigest())
 
-
-
-
-
-di	= sys.path[0]
-with open(di + "/addons.xml") as fi:
-	with open(di + "/addons.xml.md5", "w") as fo:
-		fo.write(hashlib.md5(fi.read()).hexdigest())
-
+main()
